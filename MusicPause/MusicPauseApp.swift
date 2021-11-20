679c530e88e9ff428ad1c034001d50fcb3aa0123
Andres Haro <hvaandres@icloud.com>
2021-11-19 21:20:34 -0700

    Initial Commit

---
+++ MusicPause/MusicPauseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPauseApp.swift
+//  MusicPause
+//
+//  Created by Andres Haro on 11/19/21.
+//
+
+import SwiftUI
+
+@main
+struct MusicPauseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
